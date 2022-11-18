90b75ad42f098306c9ad4aeb6ade9f6cf5f674fe
dkqp <ltg0513@gmail.com>
2022-11-18 23:18:21 +0900

    Fix/api
    minor fix

--- spec/02-bead-test.js
+++ spec/02-bead-test.js
@@ -35,9 +35,10 @@ describe('02. Bead test', () => {
         expect(res.body.data.page).to.deep.include({
           _id: '6374956747bbe1f1e03519ff',
           active: true,
-          domain: 'domain1',
-          url: 'url1',
-          title: 'page1',
+          domain: 'pks2974.medium.com',
+          url: 'https://pks2974.medium.com/iframe-간단-정리하기-1cd866b71c8f',
+          title:
+            'iframe 간단 정리하기. iframe 을 사용해볼 일이 생겨, 이참에 잘 모르고 있던… | by 박성룡 ( Andrew park ) | Medium',
           keywords: ['keyword1'],
           refs: [],
           backRefs: [],
